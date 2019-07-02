e297cf1134332d285a815e92f27fac9fa709d49b
bully12 <me@bully12.com>
2019-07-02 13:59:28 +0200

    i big of a change

--- checker_data.py
+++ checker_data.py
@@ -14,17 +14,20 @@ class Checker_data:
 
 	def init_board(self):
 		self.board = np.zeros((self.rows, self.cols))
-		for row in range(self.rows):
-			for col in range(self.cols):
-				if row%2 == col%2:
-					if row<3:
-						self.board[row,col] = self.black
-					elif row>4:
-						self.board[row,col] = self.white
-					else:
-						self.board[row,col] = self.empty
-				else:
-					self.board[row,col] = self.empty
+		# for row in range(self.rows):
+		# 	for col in range(self.cols):
+		# 		if row%2 == col%2:
+		# 			if row<3:
+		# 				self.board[row,col] = self.black
+		# 			elif row>4:
+		# 				self.board[row,col] = self.white
+		# 			else:
+		# 				self.board[row,col] = self.empty
+		# 		else:
+		# 			self.board[row,col] = self.empty
+		self.board[4,2] = self.white
+		self.board[3,3] = self.black
+		self.board[1,5] = self.black
 
 	def get_simple_player_moves(self, simple_player):
 		moves = []
@@ -33,16 +36,28 @@ class Checker_data:
 
 		if simple_player == self.white:
 			for row, col in simple_positions:
-				if self.can_move_left(simple_player,row,col):
-					moves.append(Checkers_move(row, col, row-1, col-1))
-				if self.can_move_right(simple_player,row,col):
-					moves.append(Checkers_move(row, col, row-1, col+1))
+				if self.can_jump_left(simple_player,row,col):
+					moves.append(Checkers_move(row,col,row-2,col-2))
+				if self.can_jump_right(simple_player,row,col):
+					moves.append(Checkers_move(row,col,row-2,col+2))
+			if len(moves)==0:
+				for row, col in simple_positions:
+					if self.can_move_left(simple_player,row,col):
+						moves.append(Checkers_move(row, col, row-1, col-1))
+					if self.can_move_right(simple_player,row,col):
+						moves.append(Checkers_move(row, col, row-1, col+1))
 		if simple_player == self.black:
 			for row, col in simple_positions:
-				if self.can_move_left(simple_player,row,col):
-					moves.append(Checkers_move(row, col, row+1, col-1))
-				if self.can_move_right(simple_player,row,col):
-					moves.append(Checkers_move(row, col, row+1, col+1))
+				if self.can_jump_left(simple_player,row,col):
+					moves.append(Checkers_move(row,col,row+2,col-2))
+				if self.can_jump_right(simple_player,row,col):
+					moves.append(Checkers_move(row,col,row+2,col+2))
+			if len(moves)==0:
+				for row, col in simple_positions:
+					if self.can_move_left(simple_player,row,col):
+						moves.append(Checkers_move(row, col, row+1, col-1))
+					if self.can_move_right(simple_player,row,col):
+						moves.append(Checkers_move(row, col, row+1, col+1))
 
 		return moves
 
@@ -53,6 +68,26 @@ class Checker_data:
 				if self.board[row,col] == player:
 					simple_positions.append((row,col))
 		return simple_positions
+
+	def can_jump_left(self,simple_player,row,col):
+		if simple_player == self.white:
+			if col-2>-1 and row-2>-1 and self.board[row-1,col-1] == self.black and self.board[row-2,col-2] == self.empty:
+				return True
+			return False
+		if simple_player == self.black:
+			if col-2>-1 and row+2<8 and self.board[row+1,col-1] == self.white and self.board[row+2,col-2] == self.empty:
+				return True
+			return False
+
+	def can_jump_right(self,simple_player,row,col):
+		if simple_player == self.white:
+			if col+2<8 and row-2>-1 and self.board[row-1,col+1] == self.black and self.board[row-2,col+2] == self.empty:
+				return True
+			return False
+		if simple_player == self.black:
+			if col+2<8 and row+2<8 and self.board[row+1,col+1] == self.white and self.board[row+2,col+2] == self.empty:
+				return True
+			return False
 
 	def can_move_left(self,simple_player,row,col):
 		if simple_player == self.white:
@@ -73,3 +108,38 @@ class Checker_data:
 			if col+1<8 and self.board[row+1, col+1] == self.empty:
 				return True
 			return False
+
+	def make_move(self,simple_player,r1,c1,r2,c2):
+		if simple_player == self.white:
+			simple_moves = self.get_simple_player_moves(simple_player)
+			simple_moves2 = []
+			for pos in simple_moves:
+				simple_moves2.append((pos.from_row,pos.from_col,pos.to_row,pos.to_col))
+			if (r1,c1,r2,c2) in simple_moves2:
+				self.board[r2,c2] = self.board[r1,c1]
+				self.board[r1,c1] = self.empty
+				if r1-r2 == 2 or r1-r2 == -2:
+					eatr = int((r1+r2)/2)
+					eatc = int((c1+c2)/2)
+					self.board[eatr,eatc] = self.empty
+				if r2==0 and self.board[r2,c2] == self.white:
+					self.board[r2,c2] = self.white_king
+			else:
+				print("invalid move")
+
+		elif simple_player == self.black:
+			simple_moves = self.get_simple_player_moves(simple_player)
+			simple_moves2 = []
+			for pos in simple_moves:
+				simple_moves2.append((pos.from_row,pos.from_col,pos.to_row,pos.to_col))
+			if (r1,c1,r2,c2) in simple_moves2:
+				self.board[r2,c2] = self.board[r1,c1]
+				self.board[r1,c1] = self.empty
+				if r1-r2 == 2 or r1-r2 == -2:
+					eatr = int((r1+r2)/2)
+					eatc = int((c1+c2)/2)
+					self.board[eatr,eatc] = self.empty
+				if r2==7 and self.board[r2,c2] == self.black:
+					self.board[r2,c2] = self.black_king
+			else:
+				print("invalid move")
--- main.py
+++ main.py
@@ -22,14 +22,29 @@ def print_board(b):
 if __name__ == '__main__':
 
 	board_data = Checker_data()
-	print_board(board_data)
-	white_moves = board_data.get_simple_player_moves(board_data.white)
-	black_moves = board_data.get_simple_player_moves(board_data.black)
-	print("len white_moves",len(white_moves))
-	print("white_moves")
-	for item in white_moves:
-		print(item.from_row,item.from_col,item.to_row,item.to_col)
-	print("len black_moves",len(black_moves))
-	print("black_moves")
-	for item in black_moves:
-		print(item.from_row,item.from_col,item.to_row,item.to_col)
+	trigger = 1
+	while True:
+		if trigger==1:
+			print_board(board_data)
+		try:
+			white_moves = board_data.get_simple_player_moves(board_data.white)
+			print("len white_moves",len(white_moves))
+			print("white_moves")
+			for item in white_moves:
+				print(item.from_row,item.from_col,item.to_row,item.to_col)
+			r1,c1,r2,c2 = input("White player Enter One oF the Moves above: ").split(" ")
+			board_data.make_move(board_data.white,int(r1),int(c1),int(r2),int(c2))
+			print_board(board_data)
+
+			black_moves = board_data.get_simple_player_moves(board_data.black)
+			print("len black_moves",len(black_moves))
+			print("black_moves")
+			for item in black_moves:
+				print(item.from_row,item.from_col,item.to_row,item.to_col)
+			r1,c1,r2,c2 = input("Black player Enter One oF the Moves above: ").split(" ")
+			board_data.make_move(board_data.black, int(r1),int(c1),int(r2),int(c2))
+			print_board(board_data)
+			trigger=0
+
+		except KeyboardInterrupt:
+			break
